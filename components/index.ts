7d8b852512ddd7a9e36e8235cc317c120d225d15
shakkir nm <shakkirnm@gmail.com>
2023-09-19 13:21:51 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomeFilter from "./CustomeFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  CustomeFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
